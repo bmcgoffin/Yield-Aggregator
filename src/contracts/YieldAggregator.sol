00640e378f0fdadc00ad3b218238376330df34ef
Brendan McGoffin <aloraBrendo@hotmail.com>
2021-09-22 17:24:51 -0700

    Compound Edits and Tests
    Updated compound code to handle the withdraw and also track gains made. Initial research started on AAVE. Also did tests to verify correct amount of DAI was being transmitted and the gains were being converted to DAI and properly returned to the account1.

--- src/contracts/YieldAggregator.sol
+++ src/contracts/YieldAggregator.sol
@@ -10,6 +10,8 @@ contract YieldAggregator {
     using SafeMath for uint;
 
     mapping(uint256 => _Deposit) public deposits;
+    mapping(address => uint256) public daiTokens;
+    mapping(uint256 => bool) public depositWithdrawn;
     uint256 public depositCount;
 
     event Deposited(
@@ -20,11 +22,21 @@ contract YieldAggregator {
         uint256 timestamp
     );
 
+    event Withdrawn(
+        uint256 id,
+        address user,
+        address token,
+        uint256 amount,
+        uint256 gain,
+        uint256 timestamp
+    );
+
     struct _Deposit {
         uint256 id;
         address user;
         address token;
         uint256 amount;
+        uint256 gain;
         uint256 timestamp;
     }
         
@@ -34,22 +46,17 @@ contract YieldAggregator {
     IERC20 DAI = IERC20(daiAddress);
     cDAI Compound = cDAI(cDAIAddress);
 
-    //Function to get the current DAI balance of the user
-    function getDAIBalance(address userAddress) external view returns (uint256) {
-        uint256 result = DAI.balanceOf(userAddress);
-        return (result);
-    }
-
-    function getCompoundBalance(address userAddress) external view returns (uint256) {
-        uint256 result = Compound.balanceOf(userAddress);
-        return (result);
-    }
-
     function deposit(uint _amount) public returns (bool) {
                 
+        //Create new Deposit and transfer the funds from the user account to the Smart Contract
         depositCount = depositCount.add(1);
-        deposits[depositCount] = _Deposit(depositCount, msg.sender, cDAIAddress, _amount, now);
+        deposits[depositCount] = _Deposit(depositCount, msg.sender, cDAIAddress, _amount, 0, now);
         DAI.transferFrom(msg.sender, address(this), _amount);
+        
+        //Add amount deposited to the daiToken tracker for each user
+        daiTokens[msg.sender] = daiTokens[msg.sender].add(_amount);
+
+        //Deposit to Compound
         uint deposited = depositToCompound(_amount);
 
        
@@ -71,7 +78,39 @@ contract YieldAggregator {
         // Insert Code here
     }
 
-    function withdraw() public {
+    function withdraw(uint256 _id) public {
+
+        //Get initial deposit amount and current user balance
+        _Deposit storage _deposit = deposits[_id];
+        
+        //Get current cDAI locked in Compound by this contract
+        uint256 cDaiAmount = Compound.balanceOf(address(this));
+        //Withdraw from contract
+        withdrawFromCompound(cDaiAmount, true);
+
+        uint256 daiAmount = DAI.balanceOf(address(this));
+
+        //Calculate gains while locked in contract
+        uint256 gain = daiAmount.sub(_deposit.amount);
+        _deposit.gain = gain;
+        daiTokens[msg.sender] = daiTokens[msg.sender].add(gain);
+        
+        //Get latest amount after gains added
+        uint256 userBalance = daiTokens[msg.sender];
+        //Transfer back to original account (principal plus gains)
+        DAI.transfer(_deposit.user, userBalance);
+
+        //Update trackers to account for funds withdrawn  
+        daiTokens[msg.sender] = daiTokens[msg.sender].sub(_deposit.amount);
+        depositWithdrawn[_deposit.id] = true;
+
+        //Emit withdrawn event
+        emit Withdrawn(_deposit.id, msg.sender, _deposit.token, _deposit.amount, gain, now);
+        
+        //Add in any gains from different rebalance
+
+        //With draw funds from Compound or Aave
+        //withdrawFromCompound(userBalance, false);
 
     }
 
--- test/YieldAggregator.test.js
+++ test/YieldAggregator.test.js
@@ -11,6 +11,7 @@ require('chai')
         let yieldAggregator
         let daiContract
         let compoundCDaiContract
+
         let amount = web3.utils.toWei('1', 'Ether');
 
         //Address of account on mainnet with DAI
@@ -21,12 +22,20 @@ require('chai')
         
         //Setup Compound (DAI) from the Mainnet
         const compoundDContractAddress = '0x5d3a536e4d6dbd6114cc1ead35777bab948e3643';
+        const checkSumCompoundDAddress = '0x5d3a536E4D6DbD6114cc1Ead35777bAB948E3643';
         
 
-    beforeEach(async () => {
+    before(async () => {
         yieldAggregator = await YieldAggregator.new()
         daiContract = await new web3.eth.Contract(DAI_ABI, daiMainNetAddress);
         compoundCDaiContract = await new web3.eth.Contract(COMPOUND_ABI, compoundDContractAddress);
+
+        let whaleDAIBalance = await daiContract.methods.balanceOf(daiWhale).call()
+        let userDAIBalance = await daiContract.methods.balanceOf(account1).call()
+        let contractDAIBalance = await daiContract.methods.balanceOf(yieldAggregator.address).call()
+        console.log('DAI Whale balance before withdrawl', whaleDAIBalance)
+        console.log('DAI User balance before withdrawl', userDAIBalance)
+        console.log('DAI Contract balance before withdrawl', contractDAIBalance)
 
         let transferred = await daiContract.methods.transfer(account1, amount).send({from: daiWhale})           
         let approval = await daiContract.methods.approve(yieldAggregator.address, amount).send({from: account1})
@@ -46,17 +55,85 @@ require('chai')
 
     describe('Deposits to Compound or AAVE', () => {
       let transferResult
-      
-      beforeEach(async () => {
+      let currentcDAIBalance
+      let newCompoundAmount
+      let event
+
+      before(async () => {
+        currentcDAIBalance = await compoundCDaiContract.methods.balanceOf(yieldAggregator.address).call()
+        console.log('Current of cDAI returned by Compound', currentcDAIBalance)
           transferResult = await yieldAggregator.deposit(amount, {from: account1})
         })
+        
         it('Emits a deposit event', async () => {
           const log = transferResult.logs[0]
           log.event.should.eq('Deposited')
-          const event = log.args
+          event = log.args
           event.user.should.equal(account1)
-          event.token.should.equal(compoundDContractAddress)
+          event.token.should.equal(checkSumCompoundDAddress.toString())
           event.amount.toString().should.equal(amount.toString())
+          event.timestamp.toString().length.should.be.at.least(1, 'timestamp is present')
         })
+          
+        it('Tracks new Compound amount', async () => {
+
+          newCompoundAmount = await compoundCDaiContract.methods.balanceOf(yieldAggregator.address).call()
+          let newInt = parseInt(newCompoundAmount)
+          let previousInt = parseInt(currentcDAIBalance)
+          console.log('Amount of cDAI returned by Compound', newCompoundAmount)
+          newInt.should.be.above(previousInt)
+        })
+      
+
+        describe('Withdraws funds from YieldAggregator', () => {
+          let withdrawResult
+          let currentcDAIBalance
+          let userDAIBalance
+          let contractDAIBalance
+          let depositWithdrawn
+
+          before(async () => {
+              currentcDAIBalance = await compoundCDaiContract.methods.balanceOf(yieldAggregator.address).call()
+              console.log('Before Withdrawl Current of cDAI returned by Compound', currentcDAIBalance)
+              withdrawResult = await yieldAggregator.withdraw(event.id, {from: account1})
+              currentcDAIBalance = await compoundCDaiContract.methods.balanceOf(yieldAggregator.address).call()
+              console.log('After withdrawl of cDAI returned by Compound', contractDAIBalance)
+              
+            })
+            
+            it('Emits a withdrawn event', async () => {
+              const log = withdrawResult.logs[0]
+              log.event.should.eq('Withdrawn')
+              const withdrawnEvent = log.args
+              withdrawnEvent.user.should.equal(account1)
+              withdrawnEvent.token.should.equal(checkSumCompoundDAddress.toString())
+              withdrawnEvent.amount.toString().should.equal(amount.toString())
+              withdrawnEvent.gain.toString().length.should.be.at.least(1, 'Gains recorded')
+              console.log('Total Gains', withdrawnEvent.gain)
+              withdrawnEvent.timestamp.toString().length.should.be.at.least(1, 'timestamp is present')
+            })
+            it('Updates DepositWithdrawn', async () => {
+              depositWithdrawn = await yieldAggregator.depositWithdrawn(event.id)
+              depositWithdrawn.should.equal(true)
+              currentcDAIBalance = await compoundCDaiContract.methods.balanceOf(yieldAggregator.address).call()
+              console.log('After all of cDAI returned by Compound', contractDAIBalance)
+              let whaleDAIBalance = await daiContract.methods.balanceOf(daiWhale).call()
+              userDAIBalance = await daiContract.methods.balanceOf(account1).call()
+              contractDAIBalance = await daiContract.methods.balanceOf(yieldAggregator.address).call()
+              console.log('DAI User balance after withdrawl', whaleDAIBalance)
+              console.log('DAI User balance after withdrawl', userDAIBalance)
+              console.log('DAI Contract balance after withdrawl', contractDAIBalance)
+            })
+              /*
+            it('Tracks new Compound amount', async () => {
+
+              newCompoundAmount = await compoundCDaiContract.methods.balanceOf(yieldAggregator.address).call()
+              let newInt = parseInt(newCompoundAmount)
+              let previousInt = parseInt(currentcDAIBalance)
+              console.log('Amount of cDAI returned by Compound', newCompoundAmount)
+              newInt.should.be.above(previousInt)
+            })
+            */
           })
+    })
 })
